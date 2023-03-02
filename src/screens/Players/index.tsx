f0c2ba3217184cb6c962d2a0c6c3334369f21a80
samuel.abreu <samuel.abreu@fleetcor.com.br>
2023-03-02 20:44:51 -0300

    feature: creation of filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,17 +1,54 @@
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
-import { Container } from './styles';
+import { FlatList } from "react-native";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { useState } from "react";
 
 export function Players() {
-    return (
-        <Container>
-            <Header showBackButton />
 
-            <Highlight 
-                title='Nome da turma'
-                subtitle='Adicione a galera e separe os times'
+  const [ team, setTeam ] = useState('Time A');
+  const [ players, setPlayers ] = useState([]);
+
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight
+        title="Nome da turma"
+        subtitle="Adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input 
+          placeholder="Nome da pessoa" 
+          autoCorrect={false} 
+        />
+
+        <ButtonIcon icon="add" />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={ item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item} 
+              isActive={item === team}
+              onPress={() => setTeam(item)}
             />
+          )}
+          horizontal
+        />
 
-        </Container>
-    )
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
+
+    </Container>
+  );
 }
