cc057a2bbedc2feff534fd51bab2b3490472dd40
samuel.abreu <samuel.abreu@fleetcor.com.br>
2023-03-02 19:43:34 -0300

    feature: new group interface

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { Loading } from '@components/Loading'
 
 import theme from "./src/theme";
 
-import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
 
@@ -22,7 +22,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   );
 }
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,33 @@
+import { Container, Content, Icon } from './styles';
+
+import { Button } from '@components/Button';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Input } from '@components/Input';
+
+export function NewGroup() {
+    return (
+        <Container>
+            <Header showBackButton/>
+            
+            <Content>
+                <Icon />
+
+                <Highlight 
+                    title='Nova turma'
+                    subtitle='Cria a turma para adicionar as pessoas'
+                />
+
+                <Input  
+                    placeholder='Nome da turma'
+                />
+
+                <Button 
+                    title='Criar' 
+                    style={{ marginTop: 20 }}
+                />
+            </Content>
+
+        </Container>
+    )
+}
